d2793d4c9aee10eeab7b19675317848a14092457
valr.lipsk <89910618+valr0lipsk@users.noreply.github.com>
2023-05-02 16:46:45 +0300

    feat: scaffold welcome page (#3)
    * feat: scaffold welcome page
    
    * fix: add lint-staged
    
    * fix: fix lint-staged
    
    * refactor: remove comment from styles
    
    * fix: fix welcomePage layout
    
    * refactor: put styles in module

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -1,32 +1,36 @@
 import { FC } from 'react';
-import { Button } from 'primereact/button';
-import { useNavigate } from 'react-router-dom';
 import { ERoutes } from '@/router';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { useAuthState } from '@/modules/Authentication';
+import classes from './WelcomePage.module.scss';
+import logo from '@/assets/graphql_logo.png';
 
 const WelcomePage: FC = () => {
   const { t } = useTranslation();
   const [user] = useAuthState();
-  const navigate = useNavigate();
 
   return (
-    <div className="pt-5 text-center">
-      <h1>{t('Welcome')}</h1>
+    <div className="main absolute h-full w-full flex flex-column justify-content-center align-items-center text-center">
+      <h2 className="font-light">{t('welcomePage.welcome')}</h2>
+      <h1 className="text-5xl mt-2">GraphiQl-app</h1>
+      <img className={classes.logo + ' mb-3'} src={logo} alt="graphql" />
       {user ? (
-        <Button
-          label={t('ToMain').toString()}
-          onClick={() => {
-            navigate(ERoutes.Main);
-          }}
-        />
+        <a className="text-lg" href={'../' + ERoutes.Main}>
+          {t('welcomePage.start')}
+          <i className={classes.arrow + ' pi pi-arrow-right'}></i>
+        </a>
       ) : (
-        <Button
-          label={t('SignIn').toString()}
-          onClick={() => {
-            navigate(ERoutes.Auth);
-          }}
-        />
+        <p>
+          <Trans i18nKey="welcomePage.continue" t={t}>
+            <a
+              key="link"
+              className={classes.link + ' font-bold cursor-pointer underline text-white'}
+              href={'../' + ERoutes.Auth}>
+              Sign in
+            </a>
+            to continue
+          </Trans>
+        </p>
       )}
     </div>
   );
