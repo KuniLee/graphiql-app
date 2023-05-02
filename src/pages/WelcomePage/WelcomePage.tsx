bb2b7e58caf65f67243d9fb5ba86ef0b0fd25b91
valr.lipsk <valery.lipskaya03@gmail.com>
2023-05-02 12:34:53 +0300

    refactor: put styles in module

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import { ERoutes } from '@/router';
 import { Trans, useTranslation } from 'react-i18next';
 import { useAuthState } from '@/modules/Authentication';
-import './WelcomePage.scss';
+import classes from './WelcomePage.module.scss';
 import logo from '@/assets/graphql_logo.png';
 
 const WelcomePage: FC = () => {
@@ -13,16 +13,19 @@ const WelcomePage: FC = () => {
     <div className="main absolute h-full w-full flex flex-column justify-content-center align-items-center text-center">
       <h2 className="font-light">{t('welcomePage.welcome')}</h2>
       <h1 className="text-5xl mt-2">GraphiQl-app</h1>
-      <img className="logo mb-3" src={logo} alt="graphql" />
+      <img className={classes.logo + ' mb-3'} src={logo} alt="graphql" />
       {user ? (
         <a className="text-lg" href={'../' + ERoutes.Main}>
           {t('welcomePage.start')}
-          <i className="pi pi-arrow-right"></i>
+          <i className={classes.arrow + ' pi pi-arrow-right'}></i>
         </a>
       ) : (
         <p>
           <Trans i18nKey="welcomePage.continue" t={t}>
-            <a key="link" className="font-bold cursor-pointer underline text-white" href={'../' + ERoutes.Auth}>
+            <a
+              key="link"
+              className={classes.link + ' font-bold cursor-pointer underline text-white'}
+              href={'../' + ERoutes.Auth}>
               Sign in
             </a>
             to continue
