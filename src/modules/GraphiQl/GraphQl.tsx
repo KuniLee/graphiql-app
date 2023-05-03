8ea761653296225ab4801ff8e56ac0848f0c9629
Sitnikov <selnetys@gmail.com>
2023-05-03 21:37:00 +0500

    feat: make a request to the server

--- src/modules/GraphiQl/GraphQl.tsx
+++ src/modules/GraphiQl/GraphQl.tsx
@@ -8,6 +8,7 @@ import { Button } from 'primereact/button';
 import { openDocs } from '@/modules/Documentation';
 import { useAppDispatch } from '@/hooks/redux';
 import cx from 'classnames';
+import RequestBtn from '@/modules/GraphiQl/components/RequestBtn';
 
 const GraphQl: FC = () => {
   const dispatch = useAppDispatch();
@@ -16,8 +17,8 @@ const GraphQl: FC = () => {
     <div className={classes.wrapper}>
       <div className="flex p-2 gap-2">
         <Button icon="pi pi-folder-open" onClick={() => dispatch(openDocs())} />
-        <URLInput></URLInput>
-        <Button icon={<i className="pi pi-caret-right" />} />
+        <URLInput />
+        <RequestBtn />
       </div>
       <Splitter style={{ height: '500px' }}>
         <SplitterPanel className={cx(classes.panel, 'flex align-items-center justify-content-center')}>
--- src/modules/GraphiQl/store/GraphiQlSliceSlice.ts
+++ src/modules/GraphiQl/store/GraphiQlSliceSlice.ts
@@ -1,28 +1,43 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '@/store';
+import type { ApolloClient, NormalizedCacheObject } from '@apollo/client';
+import { gql } from '@apollo/client';
 
 interface GraphiQlState {
   serverUrl: string;
   request: string;
   response: string;
+  isLoading: boolean;
 }
 
 const initialState: GraphiQlState = {
   serverUrl: 'https://graphqlzero.almansi.me/api',
-  request: '',
+  isLoading: false,
+  request: `query{
+  users{
+    data{
+      name
+    }
+  }
+}`,
   response: '',
 };
 
-// !!!This code is needed
-//
-export const apolloQuery = createAsyncThunk<void, never, { rejectValue: string }>(
+export const apolloQuery = createAsyncThunk<string, ApolloClient<NormalizedCacheObject>, { rejectValue: string }>(
   'graphiQl/query',
-  async (_, { getState }) => {
+  async (client, { getState, rejectWithValue }) => {
     const {
       graphiQl: { request },
     } = getState() as RootState;
+    const query = gql(request);
 
-    console.log(request);
+    try {
+      const result = await client.query({ query });
+
+      return JSON.stringify(result, null, ' ');
+    } catch (error) {
+      return rejectWithValue(JSON.stringify(error, null, ' '));
+    }
   }
 );
 
@@ -36,6 +51,20 @@ export const GraphiQlSlice = createSlice({
     setRequest(state, { payload }: PayloadAction<string>) {
       state.request = payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(apolloQuery.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.response = action.payload;
+      })
+      .addCase(apolloQuery.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(apolloQuery.rejected, (state, action) => {
+        state.isLoading = false;
+        state.response = action.payload || '';
+      });
   },
 });
 
