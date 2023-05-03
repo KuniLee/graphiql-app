92377d40d99ecbc79c3f5acb5d163403e2ac2143
Sitnikov <selnetys@gmail.com>
2023-05-03 20:35:01 +0500

    feat: change panel layout

--- src/modules/GraphiQl/GraphQl.tsx
+++ src/modules/GraphiQl/GraphQl.tsx
@@ -1,23 +1,33 @@
 import { FC } from 'react';
-import Header from './components/Header';
+import URLInput from './components/URLInput';
 import { Splitter, SplitterPanel } from 'primereact/splitter';
 import RequestField from '@/modules/GraphiQl/components/RequestField';
 import ResponseField from '@/modules/GraphiQl/components/ResponseField';
+import classes from './GraphQl.module.scss';
+import { Button } from 'primereact/button';
+import { openDocs } from '@/modules/Documentation';
+import { useAppDispatch } from '@/hooks/redux';
+import cx from 'classnames';
 
 const GraphQl: FC = () => {
-  return (
-    <>
-      <Header></Header>
+  const dispatch = useAppDispatch();
 
+  return (
+    <div className={classes.wrapper}>
+      <div className="flex p-2 gap-2">
+        <Button icon="pi pi-folder-open" onClick={() => dispatch(openDocs())} />
+        <URLInput></URLInput>
+        <Button icon={<i className="pi pi-caret-right" />} />
+      </div>
       <Splitter style={{ height: '500px' }}>
-        <SplitterPanel className="flex align-items-center justify-content-center">
+        <SplitterPanel className={cx(classes.panel, 'flex align-items-center justify-content-center')}>
           <RequestField />
         </SplitterPanel>
-        <SplitterPanel className="flex align-items-center justify-content-center">
+        <SplitterPanel className={cx(classes.panel, 'flex align-items-center justify-content-center')}>
           <ResponseField />
         </SplitterPanel>
       </Splitter>
-    </>
+    </div>
   );
 };
 
