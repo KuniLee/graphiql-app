22ac0860c1ca94788060975815ba8622d42683c1
valr.lipsk <valery.lipskaya03@gmail.com>
2023-05-03 14:13:14 +0300

    feat: add graphql support

--- src/modules/GraphiQl/components/Editor/Editor.tsx
+++ src/modules/GraphiQl/components/Editor/Editor.tsx
@@ -1,18 +1,25 @@
-import { useRef, useEffect, MutableRefObject } from 'react';
+import { useRef, useEffect, MutableRefObject, FC } from 'react';
 import { basicSetup } from 'codemirror';
 import { EditorState } from '@codemirror/state';
 import { EditorView, keymap } from '@codemirror/view';
-import { defaultKeymap } from '@codemirror/commands';
+import { defaultKeymap, indentWithTab } from '@codemirror/commands';
 import { solarizedDark } from 'cm6-theme-solarized-dark';
-import { javascript } from '@codemirror/lang-javascript';
+import { graphql } from 'cm6-graphql';
+import { GraphQLSchema } from 'graphql';
 
-const Editor = () => {
+type EditorProps = {
+  schema?: GraphQLSchema;
+};
+
+const Editor: FC<EditorProps> = ({ schema }) => {
   const editor = useRef<HTMLDivElement>();
 
   useEffect(() => {
     const startState = EditorState.create({
-      doc: '',
-      extensions: [basicSetup, keymap.of(defaultKeymap), solarizedDark, javascript()],
+      doc: `mutation mutationName {
+        setString(value: "newString")
+        }`,
+      extensions: [basicSetup, keymap.of([...defaultKeymap, indentWithTab]), solarizedDark, graphql(schema)],
     });
 
     const view = new EditorView({ state: startState, parent: editor.current });
@@ -20,7 +27,7 @@ const Editor = () => {
     return () => {
       view.destroy();
     };
-  }, []);
+  }, [schema]);
 
   return <div ref={editor as MutableRefObject<HTMLDivElement>}></div>;
 };
