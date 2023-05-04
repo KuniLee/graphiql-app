2b86294506a7360a72ab5fdd744481c49828fbba
valr.lipsk <valery.lipskaya03@gmail.com>
2023-05-04 13:34:18 +0300

    feat: add custom theme for editor

--- src/modules/GraphiQl/components/Editor/Editor.tsx
+++ src/modules/GraphiQl/components/Editor/Editor.tsx
@@ -3,23 +3,56 @@ import { basicSetup } from 'codemirror';
 import { EditorState } from '@codemirror/state';
 import { EditorView, keymap } from '@codemirror/view';
 import { defaultKeymap, indentWithTab } from '@codemirror/commands';
-import { solarizedDark } from 'cm6-theme-solarized-dark';
 import { graphql } from 'cm6-graphql';
 import { GraphQLSchema } from 'graphql';
+import styles from './Editor.module.scss';
 
 type EditorProps = {
   schema?: GraphQLSchema;
+  defaultValue?: string;
 };
 
-const Editor: FC<EditorProps> = ({ schema }) => {
+const customTheme = EditorView.theme({
+  '&': {
+    backgroundColor: '#0e1315',
+    borderRadius: '6px',
+    padding: '15px',
+    color: '#fff',
+    height: '100%',
+  },
+  '.cm-gutters': {
+    backgroundColor: '#0e1315',
+    borderRight: 'none',
+    color: '#fff',
+  },
+  '.cm-activeLine, .cm-activeLineGutter': {
+    backgroundColor: 'var(--gray-700)',
+  },
+  '.ͼb': {
+    color: '#D60590',
+  },
+  '.ͼc': {
+    color: '#009FB8',
+  },
+  '.cm-line': {
+    color: '#007DEB',
+  },
+  '.ͼ1.cm-focused .cm-matchingBracket,.cm-matchingBracket': {
+    backgroundColor: 'var(--primary-300) !important',
+  },
+  '.cm-scroller': {
+    lineHeight: '1.7',
+    fontSize: '1.15em',
+  },
+});
+
+const Editor: FC<EditorProps> = ({ schema, defaultValue }) => {
   const editor = useRef<HTMLDivElement>();
 
   useEffect(() => {
     const startState = EditorState.create({
-      doc: `mutation mutationName {
-        setString(value: "newString")
-        }`,
-      extensions: [basicSetup, keymap.of([...defaultKeymap, indentWithTab]), solarizedDark, graphql(schema)],
+      doc: defaultValue || '',
+      extensions: [basicSetup, keymap.of([...defaultKeymap, indentWithTab]), customTheme, graphql(schema)],
     });
 
     const view = new EditorView({ state: startState, parent: editor.current });
@@ -27,9 +60,9 @@ const Editor: FC<EditorProps> = ({ schema }) => {
     return () => {
       view.destroy();
     };
-  }, [schema]);
+  }, [defaultValue, schema]);
 
-  return <div ref={editor as MutableRefObject<HTMLDivElement>}></div>;
+  return <div className={styles.container} ref={editor as MutableRefObject<HTMLDivElement>}></div>;
 };
 
 export default Editor;
