e5217410ac1ef57746fe43e42f3085b707872898
valr.lipsk <valery.lipskaya03@gmail.com>
2023-05-04 14:07:42 +0300

    refactor: refactor to use react-codemirror

--- src/modules/GraphiQl/components/Editor/Editor.tsx
+++ src/modules/GraphiQl/components/Editor/Editor.tsx
@@ -1,68 +1,62 @@
-import { useRef, useEffect, MutableRefObject, FC } from 'react';
-import { basicSetup } from 'codemirror';
-import { EditorState } from '@codemirror/state';
+import { FC } from 'react';
+import CodeMirror from '@uiw/react-codemirror';
 import { EditorView, keymap } from '@codemirror/view';
 import { defaultKeymap, indentWithTab } from '@codemirror/commands';
 import { graphql } from 'cm6-graphql';
 import { GraphQLSchema } from 'graphql';
-import styles from './Editor.module.scss';
+import './Editor.scss';
 
 type EditorProps = {
   schema?: GraphQLSchema;
   defaultValue?: string;
+  handleChange?: (e: string) => void;
 };
 
-const customTheme = EditorView.theme({
-  '&': {
-    backgroundColor: '#0e1315',
-    borderRadius: '6px',
-    padding: '15px',
-    color: '#fff',
-    height: '100%',
-  },
-  '.cm-gutters': {
-    backgroundColor: '#0e1315',
-    borderRight: 'none',
-    color: '#fff',
-  },
-  '.cm-activeLine, .cm-activeLineGutter': {
-    backgroundColor: 'var(--gray-700)',
-  },
-  '.ͼb': {
-    color: '#D60590',
-  },
-  '.ͼc': {
-    color: '#009FB8',
-  },
-  '.cm-line': {
-    color: '#007DEB',
-  },
-  '.ͼ1.cm-focused .cm-matchingBracket,.cm-matchingBracket': {
-    backgroundColor: 'var(--primary-300) !important',
-  },
-  '.cm-scroller': {
-    lineHeight: '1.7',
-    fontSize: '1.15em',
-  },
-});
+const Editor: FC<EditorProps> = ({ schema, defaultValue, handleChange }) => {
+  const customTheme = EditorView.theme({
+    '&': {
+      backgroundColor: '#0e1315',
+      borderRadius: '6px',
+      padding: '15px',
+      color: '#fff',
+    },
+    '.cm-gutters': {
+      backgroundColor: '#0e1315',
+      borderRight: 'none',
+      color: '#fff',
+    },
+    '.cm-activeLine, .cm-activeLineGutter': {
+      backgroundColor: 'var(--gray-700)',
+    },
+    '.ͼb': {
+      color: '#D60590',
+    },
+    '.ͼc': {
+      color: '#009FB8',
+    },
+    '.cm-line': {
+      color: '#007DEB',
+    },
+    '.ͼ1.cm-focused .cm-matchingBracket,.cm-matchingBracket': {
+      backgroundColor: 'var(--primary-300) !important',
+    },
+    '.cm-scroller': {
+      lineHeight: '1.7',
+      fontSize: '1.15em',
+    },
+  });
 
-const Editor: FC<EditorProps> = ({ schema, defaultValue }) => {
-  const editor = useRef<HTMLDivElement>();
+  const extensions = [keymap.of([...defaultKeymap, indentWithTab]), customTheme, graphql(schema)];
 
-  useEffect(() => {
-    const startState = EditorState.create({
-      doc: defaultValue || '',
-      extensions: [basicSetup, keymap.of([...defaultKeymap, indentWithTab]), customTheme, graphql(schema)],
-    });
-
-    const view = new EditorView({ state: startState, parent: editor.current });
-
-    return () => {
-      view.destroy();
-    };
-  }, [defaultValue, schema]);
-
-  return <div className={styles.container} ref={editor as MutableRefObject<HTMLDivElement>}></div>;
+  return (
+    <CodeMirror
+      value={defaultValue}
+      extensions={extensions}
+      theme={customTheme}
+      basicSetup={{ autocompletion: true }}
+      onChange={handleChange}
+    />
+  );
 };
 
 export default Editor;
