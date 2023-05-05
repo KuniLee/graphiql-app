b15b2d26af19f2d2b99ca6cf62f29f3962e573db
kunilee <selnetys@gmail.com>
2023-05-05 10:46:57 +0500

    fix: fix welcome page links

--- src/pages/WelcomePage/WelcomePage.tsx
+++ src/pages/WelcomePage/WelcomePage.tsx
@@ -5,6 +5,7 @@ import { useAuthState } from '@/modules/Authentication';
 import classes from './WelcomePage.module.scss';
 import logo from '@/assets/graphql_logo.png';
 import cx from 'classnames';
+import { Link } from 'react-router-dom';
 
 const WelcomePage: FC = () => {
   const { t } = useTranslation();
@@ -16,19 +17,16 @@ const WelcomePage: FC = () => {
       <h1 className="text-3xl md:text-5xl mt-2">GraphiQl-app</h1>
       <img className={classes.logo + ' rotation mb-3'} src={logo} alt="graphql" />
       {user ? (
-        <a className="text-lg" href={'../' + ERoutes.Main}>
+        <Link className="text-lg" to={'../' + ERoutes.Main}>
           {t('welcomePage.start')}
           <i className={classes.arrow + ' pi pi-arrow-right'}></i>
-        </a>
+        </Link>
       ) : (
         <p>
           <Trans i18nKey="welcomePage.continue" t={t}>
-            <a
-              key="link"
-              className={classes.link + ' font-bold cursor-pointer underline text-white'}
-              href={'../' + ERoutes.Auth}>
+            <Link className={classes.link + ' font-bold cursor-pointer underline text-white'} to={'../' + ERoutes.Auth}>
               Sign in
-            </a>
+            </Link>
             to continue
           </Trans>
         </p>
