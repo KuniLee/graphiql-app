13bf1a5afbcef674ffb1a2935ca8cc2e8badf643
Sitnikov <selnetys@gmail.com>
2023-05-14 17:00:42 +0500

    feat: add mutations support

--- src/modules/GraphiQl/store/apolloQuery.ts
+++ src/modules/GraphiQl/store/apolloQuery.ts
@@ -3,21 +3,35 @@ import { gql, ApolloError } from '@apollo/client';
 import type { NormalizedCacheObject, ApolloClient } from '@apollo/client';
 import { RootState } from '@/store';
 import parseJson from '../helpers/parseJson';
+import type { OperationDefinitionNode } from 'graphql/language';
 
 export default createAsyncThunk<string, ApolloClient<NormalizedCacheObject>, { rejectValue: string }>(
   'graphiQl/query',
   async (client, { getState, rejectWithValue }) => {
     const {
-      graphiQl: { request, variables },
+      graphiQl: { request, variables: vars },
     } = getState() as RootState;
 
     const query = gql(request);
+    const variables = parseJson(vars);
+    const defNode = query.definitions[0] as OperationDefinitionNode;
 
     try {
-      const { data } = await client.query({ query, variables: parseJson(variables) });
+      let result;
+
+      switch (defNode.operation) {
+        case 'query':
+          result = await client.query({ query, variables });
+          break;
+        case 'mutation':
+          result = await client.mutate({ mutation: query, variables });
+      }
+
+      const data = result?.data;
 
       return JSON.stringify({ data }, null, ' ');
     } catch (error) {
+      console.dir(error);
       if (error instanceof ApolloError)
         if (error.networkError && 'result' in error.networkError)
           return JSON.stringify(error.networkError.result, null, ' ');
